652d1dfa519d2ae702cd95cb7da4f8bf11c4b35d
ezecanziani <ezecanziani@gmail.com>
2026-07-16 19:24:58 -0300

    estructura base (html ccs y style)

---
+++ src/app/components/appointment-stepper/appointment-stepper.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment-stepper',
+  imports: [],
+  templateUrl: './appointment-stepper.html',
+  styleUrl: './appointment-stepper.css'
+})
+export class AppointmentStepper {
+
+}
